ecd23043a7ebae3c1bbf1b9b0d986b3785f789aa
Vinicius Santos <viny.jr@hotmail.com>
2026-05-22 23:41:37 -0300

    feat: configure typeorm with postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule, 
+    TasksModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: parseInt(configService.get('DB_PORT', '5432')),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          autoLoadEntities: true, // carrega entidades @Entity() automaticamente
+          synchronize: true, // Mantenha false em produção
+        }
+      }
+    })
+      
+  ],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
