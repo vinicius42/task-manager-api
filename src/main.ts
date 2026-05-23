823b604b360ffe48868c566b810903416b1a9bee
Vinicius Santos <viny.jr@hotmail.com>
2026-05-22 23:48:16 -0300

    feat: configure global ValidationPipe with transform and whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true, //transform: true — ativa a transformação automática (o class-transformer)
+    whitelist: true //whitelist: true — rejeita campos que não estão no DTO
+  }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
