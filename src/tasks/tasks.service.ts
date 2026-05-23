cc41a2c8a58e1f3d42dad4e0ab1569e5a6f411e6
Vinicius Santos <viny.jr@hotmail.com>
2026-05-22 23:44:29 -0300

    feat: migrate tasks service to typeorm

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,78 +1,66 @@
 import { Injectable } from '@nestjs/common';
-import { createTaskRepository, deleteTaskRepository, getAllTasksRepository, getTaskByIdRepository, updateTaskRepository } from "../repositories/task.repository";
-import { getUserByIdRepository } from "../repositories/user.repository";
 import type { CreateTaskInput, Task, UpdateTaskInput } from "../types/task.types";
 import { err, ok, type Result } from "../utils/result";
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskEntity } from './entity/task.entity';
+import { Repository } from 'typeorm';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class TasksService {
-    createTask(input: CreateTaskInput): Result<Task>{
-        if(input.title == ''){
-            return err('Title is required')
-        }
-        if(input.title.length > 100){
-            return err('Title cannot exceed 100 characters')
-        }
-        if(input.priority != 'low' && input.priority != 'medium' && input.priority != 'high'){
-            return err('Priority must be low, medium or high')
-        }
-    
-        const userId = getUserByIdRepository(input.userId);
-        if(!userId.success){
+    constructor(
+        @InjectRepository(TaskEntity)
+        private readonly taskRepository: Repository<TaskEntity>,
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ){}
+    async createTask(input: CreateTaskInput): Promise<Result<Task>>{
+        const getUser = await this.userRepository.findOneBy({ id: input.userId });
+        if(!getUser){
             return err('User not found')
         }
-    
-        const data: Task = {
-            id: crypto.randomUUID(),
-            title: input.title,
-            description: input.description,
-            priority: input.priority,
-            status: 'to-do',
-            createDate: new Date().toISOString(),
-            updateDate: new Date().toISOString(),
-            completedAt: null,
-            userId: input.userId
-        }
-    
-        if(createTaskRepository(data).success){
-            return ok(data);
-        } else {
-            return err('Error creating task')
-        }
+        const create = this.taskRepository.create(input);
+        create.createDate = new Date().toISOString();
+        create.updateDate = new Date().toISOString();
+        create.priority = input.priority ?? 'low';
+        create.status = 'to-do';
+        return ok(await this.taskRepository.save(create));
     }
     
-    getAllTasks(): Result<Task[]>{
-        return getAllTasksRepository();
+    async getAllTasks(): Promise<Result<Task[]>>{
+        const tasks = await this.taskRepository.find();
+        return ok(tasks)
     }
     
-    getTaskById(id: string): Result<Task>{
-        return getTaskByIdRepository(id);
-    }
-    
-    updateTask(id: string, task: UpdateTaskInput): Result<Task>{
-        if(task.title == ''){
-            return err('Title is required')
-        }
-        if(task.title && task.title?.length > 100){
-            return err('Title cannot exceed 100 characters')
-        }
-        if(task.priority !== undefined && task.priority != 'low' && task.priority != 'medium' && task.priority != 'high'){
-            return err('Priority must be low, medium or high')
-        }
-    
-        if(task.status !== undefined && task.status != 'to-do' && task.status != 'in-progress' && task.status != 'done'){
-            return err('Status must be to-do, in-progress or done')
-        }
-    
-        const getTaskId = getTaskByIdRepository(id);
-        if(!getTaskId.success){
+    async getTaskById(id: string): Promise<Result<Task>>{
+        const taskById = await this.taskRepository.findOneBy({ id });
+        if(!taskById){
             return err('Task not found')
         }
-    
-        return updateTaskRepository(id, task);
+        return ok(taskById)
     }
     
-    deleteTask(id: string): Result<Task>{
-        return deleteTaskRepository(id);
+    async updateTask(id: string, task: UpdateTaskInput): Promise<Result<Task>>{
+        const taskUpdate = await this.taskRepository.findOneBy({ id });
+        if(!taskUpdate){
+            return err('Task not found')
+        }
+
+        taskUpdate.title = task.title ?? taskUpdate.title
+        taskUpdate.description = task.description ?? taskUpdate.description
+        taskUpdate.priority = task.priority ?? taskUpdate.priority
+        taskUpdate.status = task.status ?? taskUpdate.status
+        taskUpdate.updateDate = new Date().toISOString();
+
+        return ok(await this.taskRepository.save(taskUpdate));        
+    }
+    
+    async deleteTask(id: string): Promise<Result<Task>>{
+        const getTask = await this.taskRepository.findOneBy({ id });
+        if(!getTask){
+            return err('Task not found')
+        }
+        await this.taskRepository.remove(getTask);
+        return ok(getTask);
     }
 }
