d2cd0e255ba2f224077b95f03ba0e27de9a6c910
Vinicius Santos <viny.jr@hotmail.com>
2026-06-16 15:05:57 -0300

    feat: removed password response when creating an user

--- src/users/user.mapper.ts
+++ src/users/user.mapper.ts
@@ -1,11 +1,10 @@
-import { User, UserRole } from "src/types/user.types";
+import { PublicUser, UserRole } from "src/types/user.types";
 import { UserEntity } from "./entities/user.entity";
 
-export function UserEntityToUser(userEntity: UserEntity): User {
+export function UserEntityToUser(userEntity: UserEntity): PublicUser {
     return {
         id: userEntity.id,
         email: userEntity.email,
-        password: userEntity.password,
         role: userEntity.role as UserRole,
         createdAt: userEntity.createdAt
     }
